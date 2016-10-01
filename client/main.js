bd5dc2077bb2f6cf4a57a5c534e767caf8b023e6
Ahan Mukhopadhyay <Ahan@Ahans-MacBook-Pro.local>
2016-10-01 02:59:08 -0700

    Fetch Events

--- client/main.js
+++ client/main.js
@@ -4,13 +4,41 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import { Events } from '../imports/api/events.js';
 
 import './main.html';
+import './getevent.html';
 
 BlazeLayout.setRoot('body');
 
 Template.curevents.helpers({
-	events: [
-		{ text: 'Event 1', id: 1},
-		{ text: 'Event 2', id: 2},
-		{ text: 'Event 3', id: 3}
-	]
+	events: function() {
+    return Events.find({}, { sort: { createdAt: -1 }});
+  }
+});
+
+Template.getevent.events({
+  'submit .eventform'(event) {
+    console.log("Yo");
+    // Do stuff like enter this into collection
+    // Prevent default browser form submit
+    event.preventDefault();
+    
+    // Get value from form element
+    const target = event.target;
+    const text = target.url.value;
+    
+    // Insert into collection
+    Meteor.call('inserteventData', {eventurl: text});
+    
+    // Clear form
+    target.url.value = '';
+  },
+});
+
+Template.event.helpers({
+  id: function() {
+    return FlowRouter.getParam("id");
+  },
+  res: function() {
+    var id = FlowRouter.getParam("id");
+    return Events.findOne({_id: id});
+  }
 });
--- server/main.js
+++ server/main.js
@@ -1,8 +1,40 @@
 import { Meteor } from 'meteor/meteor';
-
+import FB from 'fb';
 import { Events } from '../imports/api/events.js';
 
+FB.mapi = Meteor.wrapAsync(FB.napi);
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+});
+
+Meteor.methods({
+	'inserteventData'({eventurl}) {
+		console.log("Invoked inserteventData");
+
+		if(eventurl[eventurl.length - 1] == '/'){
+			// if '/' is the last char of the url
+			eventurl = eventurl.substring(0, eventurl.length - 1);		
+		}
+
+		identifier = eventurl.substring(eventurl.lastIndexOf("/"));
+
+		console.log("identified")
+		var res = FB.mapi(identifier, {
+			access_token: "249276968799606|SbS-5WRPQ8h1YxQmHTNFaw8i3J8"
+		});
+
+		Events.insert({
+			name: res.name,
+			description: res.description,
+			loc: res.place.name,
+			lat: res.place.location.latitude,
+			lng: res.place.location.longitude,
+			time: res.start_time,
+			createdAt: new Date(),
+		});
+
+		console.log(res);
+	},
 });
