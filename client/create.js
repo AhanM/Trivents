b65dd62c88e90240529b8b71b0ecdf543db51855
Andrew George <KingsOfTyrants@gmail.com>
2016-10-01 06:52:21 -0700

    Implement dynamic dating
    The event list page now splits between events happening now and in the future. It shows "In X hours" for future events which are close to happening.

--- client/create.js
+++ client/create.js
@@ -3,38 +3,28 @@ import { Template } from 'meteor/templating'
 
 import "./create.html"
 
-Template.body.events({
+function getUnix() {
+
+}
+
+Template.create_wrapper.events({
 	'submit .create_event'(event) {
 		event.preventDefault();
 
-		console.log(event.target.eventName.value);
-
 		//constructing object data
 		const form = event.target;
+		const locale = "-0700";
 		let formData = {
 			name: form.eventName.value,
 			description: form.description.value,
 			loc: form.location.value,
 			lat: 0, //todo: use google api
 			lng: 0, //todo: use google api
-			time: 0, //todo: do correct format
+			start_time: Date.parse(form.startDate.value + "T" + form.startTime.value + locale),
+			end_time: Date.parse(form.endDate.value + "T" + form.endTime.value + locale),
 			createdAt: new Date()
 		};
+		console.log(formData);
 		Meteor.call('events.create', formData);
 	}
-});
-
-Template.create_wrapper.onCreated(function() {
-	// GoogleMaps.load({
-	// 	key: 'AIzaSyCycdCeB3hwdxOxNJralLp-RGjqn13ZAaU',
-	// 	libraries: 'places'
-	// });
-});
-
-Template.create_wrapper.onRendered(function() {
-	console.log(GoogleMaps.loaded());
-	if (GoogleMaps.loaded()) {
-		console.log('loaded')
-      $("loc").geocomplete();
-    }
 });
--- imports/api/events.js
+++ imports/api/events.js
@@ -1,3 +1,10 @@
+import { Meteor } from 'meteor/meteor'
 import { Mongo } from 'meteor/mongo';
 
 export const Events = new Mongo.Collection('events');
+
+Meteor.methods({
+	'events.create'(res) {
+		Events.insert(res);
+	}
+});
