ae7bbab7e31933cf4dcc2162c0a31bd85cb3127c
Ahan Mukhopadhyay <Ahan@Ahans-MacBook-Pro.local>
2016-10-01 01:19:30 -0700

    Basic Routing

--- client/main.js
+++ client/main.js
@@ -5,10 +5,12 @@ import { Events } from '../imports/api/events.js';
 
 import './main.html';
 
+BlazeLayout.setRoot('body');
+
 Template.curevents.helpers({
 	events: [
-		{ text: 'Event 1'},
-		{ text: 'Event 2'},
-		{ text: 'Event 3'}
+		{ text: 'Event 1', id: 1},
+		{ text: 'Event 2', id: 2},
+		{ text: 'Event 3', id: 3}
 	]
 });
